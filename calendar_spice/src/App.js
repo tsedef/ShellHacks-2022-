486039bf7c08011b5c376633ab3b8e262728599b
Marvens Mertune <marvensmertune@gmail.com>
2022-09-10 13:42:15 -0400

    Change calendar from class-based to fuction-based

---
+++ calendar_spice/src/App.js
@@ -0,0 +1,164 @@
+import React from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { useState } from "react";
+import { INITIAL_EVENTS, createEventId } from './event-utils'
+
+
+const App = () => {
+  const [weekendsVisible, toggleWeekendsVisible] = useState(true);
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  const handleWeekendsToggle = () => {
+    toggleWeekendsVisible(!weekendsVisible);
+  };
+  const handleEvents = (events) => {
+    setCurrentEvents(events);
+  };
+
+  const renderSidebar = () => {
+    return (
+      <div className="demo-app-sidebar">
+        <div className="demo-app-sidebar-section">
+          <h2>Instructions</h2>
+          <ul>
+            <li>Select dates and you will be prompted to create a new event</li>
+            <li>Drag, drop, and resize events</li>
+            <li>Click an event to delete it</li>
+          </ul>
+        </div>
+        <div className="demo-app-sidebar-section">
+          <label>
+            <input
+              type="checkbox"
+              checked={weekendsVisible}
+              onChange={handleWeekendsToggle}
+            ></input>
+            toggle weekends
+          </label>
+        </div>
+        <div className="demo-app-sidebar-section">
+          <h2>All Events ({currentEvents.length})</h2>
+          <ul>{currentEvents.map(renderSidebarEvent)}</ul>
+        </div>
+      </div>
+    );
+  };
+
+
+const renderSidebarEvent = (event) => {
+    return (
+      <li key={event.id}>
+        <b>{formatDate(event.start, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
+        <i>{event.title}</i>
+      </li>
+    )
+  }
+  
+
+
+const handleDateSelect = (selectInfo) => {
+    let title = prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
+  
+    calendarApi.unselect(); // clear date selection
+    if (title) {
+      let timeSpecificEvent = prompt(
+        "If this event is time-specific, please enter its start and end time"
+      );
+    }
+  
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
+
+
+const renderEventContent = (eventInfo) => {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    )
+  }
+  
+  const handleEventClick = (clickInfo) => {
+    if (
+      confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  };
+
+  return (
+    <div className="demo-app">
+      {renderSidebar()}
+      <div className="demo-app-main">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          weekends={weekendsVisible}
+          initialEvents={INITIAL_EVENTS} 
+        // alternatively, use the `events` setting to fetch from a feed
+          select={handleDateSelect}
+          eventContent={renderEventContent} // custom render function
+          eventClick={handleEventClick}
+          eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+          /* you can update a remote database when these fire:
+            eventAdd={function(){}}
+            eventChange={function(){}}
+            eventRemove={function(){}}
+            */
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
