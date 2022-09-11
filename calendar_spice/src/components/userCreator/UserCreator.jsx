2143e636d00c32849b3f410886574e23581744be
Elton Lucien <61792119+Elton360@users.noreply.github.com>
2022-09-10 23:54:16 -0400

    createUsers FireStore

--- calendar_spice/src/components/userCreator/UserCreator.jsx
+++ calendar_spice/src/components/userCreator/UserCreator.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { useFirestore } from "../../hooks/useFirestore";
+import { useFirestore } from "../../hooks/useFireStore";
 
 const interests = [
   "3D printing",
